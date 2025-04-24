cfdee44bd8d38144a90cc7406c793775d7655c81
Karim Hamed <karimhamed947@gmail.com>
2025-04-24 17:42:57 +0200

    [feat][APP-AppDelegate]: removing switLint warnings

---
+++ InstagramClone/Scenes/Home/View/PlayerView/PlayerView.swift
@@ -0,0 +1,20 @@
+//
+//  PlayerView.swift
+//  InstagramClone
+//
+//  Created by Karim Hamed  on 24/04/2025.
+//
+
+import UIKit
+
+class PlayerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ InstagramClone/Scenes/Home/View/ReelsCollectionViewCell/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  InstagramClone
+//
+//  Created by Karim Hamed  on 24/04/2025.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
