16018e281547e0aac4967f5d8a07516b91d53de4
Karim Hamed <karimhamed947@gmail.com>
2025-04-28 21:14:00 +0300

    [wiip][Home-ReelsCollectionViewCells]: removing awake from nib function as its not used by IGListKit , oveeriding prepare for reuse function

--- InstagramClone/Scenes/Home/Presentation/View/ReelsCollectionViewCell/ReelsCollectionViewCell.swift
+++ InstagramClone/Scenes/Home/Presentation/View/ReelsCollectionViewCell/ReelsCollectionViewCell.swift
@@ -5,76 +5,98 @@
 //  Created by Karim Hamed  on 24/04/2025.
 //
 
-import UIKit
+    import UIKit
 
-class ReelsCollectionViewCell: UICollectionViewCell {
+    class ReelsCollectionViewCell: UICollectionViewCell {
+        
+        var playerContainerView: UIView!
+        private var playerView: PlayerView!
+        
+        private var videoURL:String?
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            setUpPlayerContainerView()
+            setUpPlayerView()
+        }
+        
+        required init?(coder: NSCoder) {
+            super.init(coder: coder)
+        }
+        
+        private func setUpPlayerContainerView() {
+            playerContainerView = UIView()
+            playerContainerView.backgroundColor = .black
+            contentView.addSubview(playerContainerView)
+            playerContainerView.translatesAutoresizingMaskIntoConstraints = false
+            playerContainerView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+            playerContainerView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+            playerContainerView.heightAnchor.constraint(equalTo: contentView.heightAnchor).isActive = true
+            playerContainerView.widthAnchor.constraint(equalTo: contentView.widthAnchor).isActive = true
 
-    @IBOutlet weak var playerView: PlayerView!
-    @IBOutlet weak var playerContainerView: UIView!
-    private var videoURL:String?
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-    }
-
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        prepareVideo()
+        }
+        
+        private func setUpPlayerView() {
+            playerView = PlayerView()
+            playerContainerView.addSubview(playerView)
+            
+            playerView.translatesAutoresizingMaskIntoConstraints = false
+            
+            playerView.centerXAnchor.constraint(equalTo: playerContainerView.centerXAnchor).isActive = true
+            playerView.centerYAnchor.constraint(equalTo: playerContainerView.centerYAnchor).isActive = true
+            
+            playerView.widthAnchor.constraint(equalTo: playerContainerView.widthAnchor).isActive = true
+            playerView.heightAnchor.constraint(equalTo: playerContainerView.heightAnchor).isActive = true
+        }
+        
+        
+        
+        func prepareVideo() {
+            print("prepare video start")
+            
+            guard let videoURL = videoURL else {
+                print("video url nil")
+                return}
+            guard let url = URL(string: videoURL) else {
+                print("url nil")
+                
+                return }
+            playerView.prepareToPlay(with: url)
+            play()
+            print("prepare video end")
+            
+            
+        }
+        func play() {
+            print("play start")
+            playerView.player?.play()
+            print("play end")
+            
+            
+        }
+        
+        func pause() {
+            print("pause start")
+            
+            playerView.player?.pause()
+            
+            print("pause end")
+            
+        }
+        
+        func configure(model:ReelsCollectionViewCellModel){
+            videoURL = model.url
+            print("Configured with videoURL: \(videoURL ?? "nil")")
+        }
+        func cancelPreparation() {
+            playerView.cancelPreparation()
+        }
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        playerView.resetPlayerView()
+        videoURL = nil
+        
+        cancelPreparation()
+        
     }
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        print("awake from nib start")
-        playerContainerView.backgroundColor = .black
-        print("awake from nib end")
-
-    }
-    
-
-    
-    private func setUpPlayerView() {
-        playerContainerView.addSubview(playerView)
-        playerView.translatesAutoresizingMaskIntoConstraints = false
-        playerView.leadingAnchor.constraint(equalTo: playerContainerView.leadingAnchor).isActive = true
-        playerView.trailingAnchor.constraint(equalTo: playerContainerView.trailingAnchor).isActive = true
-        playerView.heightAnchor.constraint(equalTo: playerContainerView.widthAnchor).isActive = true
-        playerView.centerYAnchor.constraint(equalTo: playerContainerView.centerYAnchor).isActive = true
-
-    }
-    
-    
-    func prepareVideo() {
-        print("prepare video start")
-
-        guard let videoURL = videoURL else {
-            print("video url nil")
-            return}
-        guard let url = URL(string: videoURL) else {
-            print("url nil")
-
-            return }
-        playerView.prepareToPlay(with: url)
-        play()
-        print("prepare video end")
-
-
-    }
-    func play() {
-print("play start")
-        playerView.player?.play()
-        print("play end")
-
-
-    }
-    
-    func pause() {
-
-        playerView.player?.pause()
-
-    }
-    
-    func configure(model:ReelsCollectionViewCellModel){
-        videoURL = model.url
-          print("Configured with videoURL: \(videoURL ?? "nil")")
-    }
-
 }
